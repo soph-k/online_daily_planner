8da7332df69925aa78e2d6bfce72c20db551c3d8
Sophia Kiani <sophkiani@gmail.com>
2021-10-04 01:47:02 -0500

    updated the whole structure of the webpage

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,5 +9,36 @@
 // Date: 09/16/2021
 // Purpose: sophk_onlie_daily_calender
 
+let currentDate = $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
+let saveButtonEl = $("saveBtn");
+let currentHour = moment().hours();
+
+
+function stylingTimes () {
+  currentDate
+  $("#currentDay").append(currentDate);
+
+  $(".hour").each(() => {
+    let selectionHour = parseInt($(this).attr("data-hour"));
+    if (selectionHour > currentHour) {
+      $(this).addClass("future")
+    }
+    else if (selectionHour < currentHour) {
+      $(this).addClass("present");
+    }
+    else {
+      $(this).addClass("past");
+    }
+  })
+}
+
+saveButtonEl.on("click", function () {
+    var timeSave = $(this).parent().attr("data-time");
+    var descriptionSave = $(this).find(".description").val();
+    localStorage.setItem(timeSave, descriptionSave);
+});
+
+stylingTimes ();
+
 
 
