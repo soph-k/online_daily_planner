316413b40f5615bf34f99f8a40accc888f67a5b2
Sophia <sophkiani@gmail.com>
2021-10-28 18:51:32 -0500

    update

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,9 @@ const currentDate = $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss
 const rowHourEl = $('.row').data('time');
 const rowHourElString = $('.row').find('data-time');
 const saveButtonEl = $(".saveBtn");
-const currentHour = moment().hours("HH");
+const currentHour = moment().format("HH");
 const descriptionEl = $('.container').find(('.description'));
-const oldEvents = JSON.parse(localStorage.getItem("saved")) || [];
-
+let oldEvents = [];
 
 ///////////////////////////////// Functions ////////////////////////////////////////////////
 // Displays current date and adds styling
@@ -15,34 +14,33 @@ function stylingTimes() {
 
   $('div').each(function () {                                // Loop for each hour element to add styling... 
     const rowTime = $(this).data('time');
-    if (rowTime < currentHour) {                             // based on user's current time.
+    if (rowTime > currentHour) {                             // based on user's current time.
       $(this).addClass("future");
     }
-    else if (rowTime > currentHour) {
+    else if (rowTime < currentHour) {
       $(this).addClass("past");
     }
     else (rowTime == currentHour); {
       $(this).addClass("present");
     }
   });
-  displayReminders();
-
 }
 
 // Saves items to local storage
 function saveNotes() {
-  $('textarea').each(() => {
+  oldEvents = [];
+  $('textarea').each(function () {
     const newNotes = $(this).val();
     oldEvents.push(newNotes);                                     // Pushes in new event into old event
   });                      
   localStorage.setItem("saved", JSON.stringify(oldEvents));  // Saves old and new events in a string
-  displayReminders();
 }
 
 // Retrives items from local storage and displays them
 function displayReminders() {
-  $('textarea').each((saved) => {
-    $(this).text(saved.oldEvents)
+  oldEvents = JSON.parse(localStorage.getItem('saved'));
+  $('textarea').each(() => {
+    $(this).text(oldEvents)
   })
 }
 
