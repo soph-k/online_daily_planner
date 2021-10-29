20487af120bf746a8ad6680d3c6b5ad8378766b6
Sophia <sophkiani@gmail.com>
2021-10-28 21:59:48 -0500

    fixed js bugs, everything is now functioning

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,21 +1,16 @@
 /////////////////////////////// Global Selectors //////////////////////////////////////////
 const currentDate = $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
-const rowHourEl = $('.row').data('time');
-const rowHourElString = $('.row').find('data-time');
-const saveButtonEl = $(".saveBtn");
 const currentHour = moment().format("HH");
-const descriptionEl = $('.container').find(('.description'));
-let oldEvents = [];
 
 
 ///////////////////////////////// Functions ////////////////////////////////////////////////
 // Displays current date and adds styling
 function stylingTimes() {
-  $("#currentDay").append(currentDate);                       // Displays user current time and date.
+  $("#currentDay").append(currentDate);                            // Displays user current time and date.
 
-  $('div').each(function () {                                // Loop for each hour element to add styling... 
+  $('div').each(function () {                                     // Loop for each hour element to add styling... 
     const rowTime = $(this).data('time');
-    if (rowTime > currentHour) {                             // based on user's current time.
+    if (rowTime > currentHour) {                                  // based on user's current time.
       $(this).addClass("future");
     }
     else if (rowTime < currentHour) {
@@ -28,26 +23,21 @@ function stylingTimes() {
 }
 
 // Saves items to local storage
-function saveNotes() {
+$(".saveBtn").on('click', function () {
+  let oldEvents = [];
   $('textarea').each(function () {
-    if(!($(this).val() === "")) {
-      const newNotes = $(this).val();
-      oldEvents.push(newNotes);                                  // Pushes in new event into old event
-  }});                      
-  localStorage.setItem("saved", JSON.stringify(oldEvents));    // Saves old and new events in a string
-}
+    oldEvents.push($(this).val());                                  // Pushes in new event into old event
+  });                      
+  localStorage.setItem("saved", JSON.stringify(oldEvents));         // Saves old and new events in a string
+})
 
 // Retrives items from local storage and displays them
 function displayReminders() {
-  // oldEvents = JSON.parse(localStorage.getItem('saved'));
-  $('textarea').each(() => {
-    $(this).text(oldEvents)
+  oldEvents = JSON.parse(localStorage.getItem('saved'));           // Obtains string value from local storage
+  $('textarea').each(function (index) {
+    $(this).text(oldEvents[index])
   })
 }
-
-
-////////////////////////// Event Listeners ////////////////////////////////
-saveButtonEl.click(saveNotes);
 
 
 //////////////////////// Functions When Page Loaded //////////////////////
